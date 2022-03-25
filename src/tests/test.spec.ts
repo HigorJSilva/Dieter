21eec29aafd39de8933334eec5ef565b25641a05
Higor Silva <higor.junio@hotmail.com>
2022-03-25 11:57:10 -0300

    feat: mocha testing for http

--- src/tests/test.spec.ts
+++ src/tests/test.spec.ts
@@ -1,10 +1,52 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { Response } from 'restify';
+import server from '../server'
 
-describe('Initial Test', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-        let array = [1, 2, 3].indexOf(4)
-        expect(array).to.equal(-1)
+chai.use(chaiHttp);
+
+describe("Routes testing for/test.", function() {
+ 
+  let error: Error, response: any;
+
+  before(function(done) {
+    chai.request(server).get("/test").end(function(err, res) {
+      error = err, response = res;
+      done();
     });
   });
+
+  it("should return a success message", function() {
+    expect(response.body.status).to.equal(true);
+  });
+
+  it("should return an array", function() {
+    expect(response.body.data).to.be.an("array");
+  });
+
+});
+
+describe("Routes testing for /error-test.", function() {
+ 
+  let error: Error, response: any;
+
+  before(function(done) {
+    chai.request(server).get("/error-test").end(function(err, res) {
+      error = err, response = res;
+      done();
+    });
+  });
+
+  it("should return a false message", function() {
+    expect(response.body.status).to.equal(false);
+  });
+
+  it("should return a message", function() {
+    expect(response.body.message).to.be.an("string");
+  });
+
+  it("should return the erros", function() {
+    expect(response.body.erros).to.be.an("array");
+  });
+
 });
